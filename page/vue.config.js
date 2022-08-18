f374057d69f6f27439732724e3f7a2c9f355ba12
정인호 <newjihol@naver.com>
2022-08-18 10:58:47 +0900

    fullpage 셋팅

---
+++ page/vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+module.exports = {
+  css: {
+    sourceMap:true, 
+    loaderOptions: {
+      scss: {
+        // options here will be passed to css-loader
+        sourceMap:true
+      },
+    }
+  }
+}
